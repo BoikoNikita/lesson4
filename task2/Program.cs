e7209a7ae60fed80a992b7da743578ca99641d30
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-24 21:07:04 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumN(int N){
+    
+int a = Convert.ToString(N).Length;
+int b = 0;
+int result = 0;
+
+for (int i = 0; i < a; i++)
+{
+  b = N - N % 10;
+  result = result + (N - b);
+  N = N / 10;
+}
+return result;
+}
+
+int sumN = SumN(N);
+Console.WriteLine("Сумма цифр: " + sumN);
