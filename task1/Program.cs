8fb20f125297c6de9dd523f5d045e8a3b600acc3
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-24 18:36:42 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число A: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write ("Результат: ");
+Console.WriteLine(Math.Pow(N,M));
